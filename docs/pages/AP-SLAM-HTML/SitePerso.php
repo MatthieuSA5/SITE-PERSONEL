f7ac1e03de20222070eae23042367fbe69bf6d1d
MatthieuSA5 <matthieu.saillardaumont@sts-sio-caen.info>
2023-12-14 16:38:54 +0100

    .

--- docs/pages/AP-SLAM-HTML/SitePerso.php
+++ docs/pages/AP-SLAM-HTML/SitePerso.php
@@ -6,10 +6,11 @@
 <head>
     <title>Projet port-folio</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="./../../assetes/CSS/AP-SLAM-CSS/siteperso.css">
+    <link rel="stylesheet" href="google.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap" rel="stylesheet">
+
 </head>
     <body>
         <nav>
