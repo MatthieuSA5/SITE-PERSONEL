e1060522047fbe097d1b497f88b7037e57b4fcb0
MatthieuSA5 <matthieu.saillardaumont@sts-sio-caen.info>
2023-12-20 21:52:27 +0100

    portfolio

--- docs/pages/SitePerso.php
+++ docs/pages/SitePerso.php
@@ -1,21 +1,26 @@
+<?php
+require_once("../YAML/yaml/yaml.php");
+?>
+
 <!--<!DOCTYPE html> -->
 <html lang="fr">
 
-<!-- code N°1 -->
+<!-- code N°2 -->
 
 <head>
     <title>Projet port-folio</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../assetes/CSS/AP-SLAM-CSS/siteperso.css">
+    <link rel="stylesheet" href="../assetes/CSS/siteperso.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap" rel="stylesheet">
+
 </head>
     <body>
         <nav>
             <ul>
                 <li class="deroulant">
-                    <a href="#accueil"><i class="fa-solid fa-bars"></i></a>
+                    <a href="#accueil"></i></a>
                     <ul class="sous">
                         <li><a href="#accueil">accueil</a></li>
                         <li><a href="#a-propos">à propos</a></li>
@@ -28,16 +33,19 @@
             </ul> 
         </nav>
         
+
 <!-- section accueil -->
+<?php
+$data=yaml_parse_file("../assetes/yaml/accueil.yaml");
+?>
 
         <section id="accueil" style="background-color: rgb(36, 173, 173);">
-            <script src="../../assetes/PHP/site-perso.php"></script>
             <h1>Accueil</h1>
             <div class="contenu-accueil">
                 <div class="position-image">
-                    <img src="../../assetes/IMG/téléchargement.jfif" alt="">
+                <?php echo '<img src="../../assetes/IMG'.$data['img'].'">'; ?>
                 </div>
-                <p>Bienvenue sur le Projet Portfolio de Matthieu Saillard-Aumont</p>
+                <?php echo '<p>'.$data['accroche'].'</p>'; ?>
             </div>
             <footer>
                 <a class="arrow-color-left" href="#contact">
@@ -46,19 +54,22 @@
                 </a>
                 <a class="arrow-color-right" href="#a-propos">
                     <i class="fa-solid fa-arrow-right-long"></i>
-                    <span class="arrow-color-right">Page suivante</span></a>
+                    <span class="arrow-color-right">Page suivante</span>
+                </a>
             </footer>
         </section>
 
+
 <!-- section a propos-->     
+<?php
+$data=yaml_parse_file("../assetes/yaml/à-propo.yaml");
+?>
 
         <section id="a-propos" style="background-color: rgb(49, 4, 70);">
             <h1>à propos</h1>
             <div class="contenu-a-propos">
-                <p>Ce site Web, créé par mais propre moyen, vous donnera une vision de mes compétences.</p><br>
-                <p>Je suis étudiant en première année de BTS (brevet de technicien supérieur) SIO (Services Informatiques aux Organisations) au sein de l'établisement CaenSup Sainte-Ursule.<br>
-                    Je réalise ce site Web dans le but d'aboutir un projet en atelier professionnel.<br>
-                    je suis âgé de 18 ans est vous présentera mais compétences, mon éxperiance en milieu pro et mais différante formation</p>
+                <?php echo '<p>'.$data['compétence'].'</p>'; ?> <br>
+                <?php echo '<p>'.$data['présentation'].'</p>'; ?>
             </div>
             <footer>
                 <a class="arrow-color-left" href="#accueil">
@@ -72,7 +83,19 @@
             </footer>
         </section>
 
+
 <!-- section competences -->
+<?php
+$data=yaml_parse_file("../assetes/yaml/compétance.yaml");
+
+// Parcours des données YAML pour générer des barres de progression
+ //foreach ($donnees['barres_de_progression'] as $barre) {
+  //  echo '<div class="progress">';
+    //echo '<div class="barre" style="width: ' . $barre['progression'] . '%;"></div>';
+    //echo '<span>' . $barre['titre'] . '</span>'; // Affiche le titre de la barre
+    //echo '</div>';
+//}
+?>
 
 <section id="competences" style="background-color: rgb(86, 166, 219)">
     <h1>Compétences</h1>
@@ -91,12 +114,21 @@
     </footer>
 </section>
 
+
+<!-- section Expériance -->
+<?php
+$data=yaml_parse_file("../assetes/yaml/éxperiance.yaml");
+?>
+
+
 <section id="experience" style="background-color: rgb(194, 58, 58);">
     <h1>Expérience</h1>
     <div class="contenu-experience">
-        <tr>futur php</tr>
+       <?php echo '<p>'.$data['2020-2021'].' </p>' ?>
+       <?php echo '<p>'.$data['2021-2022'].' </p>' ?>
+       <?php echo '<p>'.$data['2022-2023'].' </p>' ?>
     </div>
-  </div>
+
     <footer>
         <a class="arrow-color-left" href="#competences">
             <span class="arrow-color-right">Page précédente</span>
@@ -109,10 +141,19 @@
     </footer>
 </section>
 
+
+<!-- section Formation -->
+<?php
+$data=yaml_parse_file("../assetes/yaml/Formation.yaml");
+?>
+
+
 <section id="formation" style="background-color: rgb(73, 10, 68);">
     <h1>Formation</h1>
     <div class="contenu-formation">
-        <tr>futur php</tr>
+        <?php echo '<p>'.$data['Brevet'].' </p>' ?> <br>
+        <?php echo '<p>'.$data['2020-2023'].' </p>' ?> <br>
+        <?php echo '<p>'.$data['BTS'].' </p>' ?>
     </div>
 
     <footer>
@@ -126,6 +167,12 @@
         </a>
     </footer>
 </section>
+
+<!-- section Contact -->
+<?php
+//$data=yaml_parse_file("../assetes/yaml/contact.yaml");
+?>
+
 
 <section id="contact" style="background-color: rgb(138, 93, 35);">
     <h1>Contact</h1>
